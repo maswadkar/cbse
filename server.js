7e290ce451dc5a3a3a945f52bf446ca2fd5f2625
Your Name <you@example.com>
2018-07-29 14:39:20 +0000

    downloaed bootstrap

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express")
-
+const path = require("path")
 
 const app = express()
+app.use(express.static(path.join(__dirname,"public")))
 
-const port = 8001
+app.use("/css",express.static(path.join(__dirname,"/node_modules/bootstrap/dist/css")))
+app.use('/js',express.static(path.join(__dirname,"/node_modules/bootstrap/dist/js")))
+app.use('/js',express.static(path.join(__dirname,"/node_modules/jquery/dist")))
+
+
+const port = 8002
 
 app.get('/',function(req,res){
-res.status(200).json({"message":"Hello World"})
+res.sendFile(path.join(__dirname,"/views","/index.html"))
 });
 
 app.listen(port,function(){
