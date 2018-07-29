4a4bae3cf6f2c1bdac09bfcd8c1f4a09a6064160
Your Name <you@example.com>
2018-07-29 17:57:55 +0000

    found category

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
-app.use(express.static(path.join(__dirname,"public")))
 
+app.use(express.static(path.join(__dirname,"public")))
 app.use("/css",express.static(path.join(__dirname,"/node_modules/bootstrap/dist/css")))
 app.use('/js',express.static(path.join(__dirname,"/node_modules/bootstrap/dist/js")))
 app.use('/js',express.static(path.join(__dirname,"/node_modules/jquery/dist")))
